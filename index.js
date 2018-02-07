cec67e811eee8a9026cc3b8e6361846fbdba703e
Sara S <ewf.gloria@gmail.com>
2018-02-07 21:38:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var katzDeli = []
+var katzDeliLine = []
 
 function takeANumber (katzDeliLine, newPerson){
   katzDeliLine.push(newPerson)
   return katzDeliLine.length + 1
+}
+function nowServing (){
+  
 }
