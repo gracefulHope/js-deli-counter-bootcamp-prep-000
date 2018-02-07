85de5b86a3392ab3347160cfb31982f54efa727b
Sara S <ewf.gloria@gmail.com>
2018-02-07 21:54:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function nowServing (katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty."
+  } else {
   var list
   var num = 0
   var person
@@ -21,4 +24,5 @@ function currentLine(katzDeliLine){
     list = list + ` ${num}. ${person},`
   }
   return `The line is currently ${list}`
+  }
 }
